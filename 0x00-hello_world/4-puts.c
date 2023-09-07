a2b1dd387e68a24f88e1c94ca9f528a88a541787
richkob <kobrich10@gmail.com>
2023-09-07 15:01:35 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main put function
- * Return: 0
- * */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-
-      puts("\"Programming is like building a multilingual puzzle");
-      return(0);
-
-
-}	
+	    puts("\"Programming is like building a multilingual puzzle");
+	        return (0);
+}
