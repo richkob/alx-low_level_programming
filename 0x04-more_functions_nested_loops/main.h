a8ee8d63fd0df78e3da7949c96c1cb90292ede88
richkob <kobrich10@gmail.com>
2023-09-14 22:44:14 +0000

    task 101

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -13,5 +11,5 @@ void print_square(int size);
 void print_triangle(int size);
 int mul(int a, int b);
 void print_number(int n);
-#endif
+
 
